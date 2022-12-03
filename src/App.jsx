49fc95f6c4422ac044ba57de42d8f3d903d4aa85
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-12-03 12:37:48 +0000

    feat: import tone into App and test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import styles from "./App.module.css";
+import { PolySynth } from "tone";
+
+
+const polySynth = new PolySynth().toDestination();
 
 function App() {
 
   return (
     <div class={styles.App} 
+      <button
+        onMouseDown={() => polySynth.triggerAttack(["C4"])}
+        onMouseUp={() => polySynth.triggerRelease(["C4"])}
+      >
+        play note
+      </button>
     </div>
   );
 }
