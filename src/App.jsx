c9d420ecc46d8e186139b174b2517804d141d343
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-12-03 12:39:48 +0000

    feat: import webmidi into App and test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import styles from "./App.module.css";
+import { WebMidi } from "webmidi";
 import { PolySynth } from "tone";
 
+WebMidi.enable()
+  .then(() => {
+    console.log("WebMidi is enabled");
+    WebMidi.inputs.forEach((input) =>
+      console.log(input.manufacturer, input.name)
+    );
+  })
+  .catch((e) => console.log(e));
 
 const polySynth = new PolySynth().toDestination();
 
