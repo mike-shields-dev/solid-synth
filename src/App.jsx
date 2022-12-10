8cf4bd35425e640f031f433745f369c66d5b8871
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-12-10 22:45:57 +0000

    feat: App correctly handles note and transposition events from qwerty keyboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { createSignal, onCleanup, onMount } from "solid-js";
 import styles from "./App.module.css";
 import { WebMidi } from "webmidi";
-import { PolySynth } from "tone";
+import * as Tone from "tone";
+import noteIndexFromChar from "./utils/noteIndexFromChar";
+import midiNoteNames from "./utils/midiNoteNamesArr";
 
 WebMidi.enable()
   .then(() => {
@@ -11,18 +14,90 @@ WebMidi.enable()
   })
   .catch((e) => console.log(e));
 
-const polySynth = new PolySynth().toDestination();
+const polySynth = new Tone.PolySynth(Tone.FMSynth);
+polySynth.toDestination();
 
 function App() {
+  const [transposition, setTransposition] = createSignal(0);
+  const [activeNotes, setActiveNotes] = createSignal([]);
+
+  const handleTransposition = (e) => {
+    if (e.type === "keyup") {
+      midiNoteNames.forEach((note) => polySynth.triggerRelease([note]));
+
+      return setTransposition((prevTrans) => {
+        const transAmount = e.key === "Z" ? -12 : e.key === "X" ? 12 : 0;
+        const transResult = prevTrans + transAmount;
+        return transResult <= 96 && transResult >= 0 ? transResult : prevTrans;
+      });
+    }
+  };
+
+  const handleNotes = (e) => {
+    const noteIndex = noteIndexFromChar(e.key);
+    if (noteIndex === false) return;
+
+    const noteName = midiNoteNames[noteIndex + transposition()];
+
+    if (noteName) {
+      if (e.type === "keydown" && !activeNotes().includes(noteName)) {
+        setActiveNotes((prev) => [...prev, noteName]);
+        polySynth.triggerAttack([noteName]);
+      }
+      if (e.type === "keyup") {
+        setActiveNotes(
+          activeNotes().filter((activeNote) => activeNote !== noteName)
+        );
+        polySynth.triggerRelease([noteName]);
+      }
+    }
+  };
+
+  const handleQwertyKeyEvents = (e) => {
+    const isNoteMode = e.getModifierState("CapsLock");
+    if (isNoteMode) {
+      const isTransposeEvent = ["Z", "X"].includes(e.key) && e.type === "keyup";
+      if (isTransposeEvent) return handleTransposition(e);
+      else handleNotes(e);
+    }
+  };
+
+  const addQwertyKeyEventListeners = () => {
+    window.addEventListener("keydown", handleQwertyKeyEvents);
+    window.addEventListener("keyup", handleQwertyKeyEvents);
+  };
+  const removeQwertyKeyEventListeners = () => {
+    window.removeEventListener("keydown", handleQwertyKeyEvents);
+    window.removeEventListener("keyup", handleQwertyKeyEvents);
+  };
+
+  onMount(() => addQwertyKeyEventListeners());
+  onCleanup(() => removeQwertyKeyEventListeners());
 
   return (
-    <div class={styles.App} 
-      <button
-        onMouseDown={() => polySynth.triggerAttack(["C4"])}
-        onMouseUp={() => polySynth.triggerRelease(["C4"])}
+    <div class={styles.App}>
+      <div>{transposition()}</div>
+      <div
+        class={styles.App_notes}
+        style={{
+          display: "flex",
+          gap: "1em",
+          margin: "0 auto",
+          border: "1px solid red",
+          "justify-content": "center",
+          "font-size": "0.5rem",
+        }}
       >
-        play note
-      </button>
+        {midiNoteNames.map((noteName) => (
+          <div
+            style={{
+              color: `${activeNotes().includes(noteName) ? "black" : "white"}`,
+            }}
+          >
+            {noteName}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
