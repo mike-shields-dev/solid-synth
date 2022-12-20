4045405904e271545c22ece3efc1a6a160857737
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-12-20 22:25:57 +0000

    refactor: Refactor code and integrate with refactored Synth

--- src/components/OctaveSpinbutton/index.jsx
+++ src/components/OctaveSpinbutton/index.jsx
@@ -1,9 +1,9 @@
 import killUIEvent from "../../utils/killUIEvent";
 import synth from "../../Synth";
 
-function OctaveSpinbutton(props) {
-  function onOctave(e) {
-    if(e.type === "keyup" && !["ArrowUp", "ArrowDown"].includes(e.key)) return;
+const OctaveSpinbutton = (props) => {
+  const onOctave = (e) => {
+    if (e.type === "keyup" && !["ArrowUp", "ArrowDown"].includes(e.key)) return;
     let adjustment;
     if (e.key === "ArrowUp") adjustment = 1;
     if (e.key === "ArrowDown") adjustment = -1;
@@ -14,9 +14,9 @@ function OctaveSpinbutton(props) {
 
     if (newOctave < synth.octaveMin || newOctave > synth.octaveMax) return;
 
-    synth.releaseAll();
+    synth.releaseActiveNotes();
     props.setOctave(newOctave);
-  }
+  };
 
   return (
     <>
@@ -36,6 +36,6 @@ function OctaveSpinbutton(props) {
       />
     </>
   );
-}
+};
 
 export default OctaveSpinbutton;
