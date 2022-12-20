643ce5dcede3c2ae979f67ea6da2bbc5ad9442a8
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-12-20 22:07:34 +0000

    refactor: Tidy up App component removing redundant code and unfinished features

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
-import { createSignal } from "solid-js";
 import styles from "./App.module.css";
 import QwertyKeyEventManager from "./components/QwertyKeyEventManager";
 import MIDIEventManager from "./components/MIDIEventManager";
 import KeyboardUI from "./components/KeyboardUI";
 import OscillatorUI from "./components/Oscillator";
-import Filter from "./components/Filter";
-import synth from "./Synth";
-
-const [octave, setOctave] = createSignal(synth.octave);
+import MasterGain from "./components/MasterGain";
 
 function App() {
   return (
     <div class={styles.App}>
       <OscillatorUI />
-      <Filter />
+      <MasterGain />
       <KeyboardUI />
       <MIDIEventManager />
-      <QwertyKeyEventManager setOctave={setOctave} />
+      <QwertyKeyEventManager />
     </div>
   );
 }
