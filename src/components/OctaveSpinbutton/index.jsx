38a33aa3824fdd2421bf4de4ec3e62a7cff4d3ef
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-12-20 12:30:34 +0000

    feat: OctaveSpinbutton only updates it's parent's octave, not synth.octave, it fires synth.releaseAll() upon octave change

--- src/components/OctaveSpinbutton/index.jsx
+++ src/components/OctaveSpinbutton/index.jsx
@@ -3,18 +3,19 @@ import synth from "../../Synth";
 
 function OctaveSpinbutton(props) {
   function onOctave(e) {
+    if(e.type === "keyup" && !["ArrowUp", "ArrowDown"].includes(e.key)) return;
     let adjustment;
     if (e.key === "ArrowUp") adjustment = 1;
     if (e.key === "ArrowDown") adjustment = -1;
-    
+
     let newOctave;
-    if(e.type === "change") newOctave = +e.target.value
-    else newOctave = synth.octave + adjustment;
+    if (e.type === "change") newOctave = +e.target.value;
+    else newOctave = props.octave + adjustment;
 
     if (newOctave < synth.octaveMin || newOctave > synth.octaveMax) return;
 
-    synth.octave = newOctave;
-    props.setOctave(synth.octave);
+    synth.releaseAll();
+    props.setOctave(newOctave);
   }
 
   return (
@@ -29,6 +30,7 @@ function OctaveSpinbutton(props) {
         onkeydown={killUIEvent}
         onkeypress={killUIEvent}
         onkeyup={onOctave}
+        oninput={killUIEvent}
         value={props.octave}
         type="number"
       />
