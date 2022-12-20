a537f08d08308d0f6ff243faf153fd1cabeb8bd0
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-12-20 12:56:26 +0000

    refactor: Delete redundant code in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { createSignal, createEffect, For } from "solid-js";
+import { createSignal } from "solid-js";
 import styles from "./App.module.css";
-import midiNotes from "./utils/midiNotes";
-import OctaveSpinbutton from "./components/OctaveSpinbutton";
 import QwertyKeyEventManager from "./components/QwertyKeyEventManager";
 import MIDIEventManager from "./components/MIDIEventManager";
 import KeyboardUI from "./components/KeyboardUI";
 import OscillatorUI from "./components/Oscillator";
+import Filter from "./components/Filter";
 import synth from "./Synth";
 
 const [octave, setOctave] = createSignal(synth.octave);
@@ -14,9 +13,10 @@ function App() {
   return (
     <div class={styles.App}>
       <OscillatorUI />
+      <Filter />
       <KeyboardUI />
       <MIDIEventManager />
-      <QwertyKeyEventManager setOctave={setOctave}  />
+      <QwertyKeyEventManager setOctave={setOctave} />
     </div>
   );
 }
