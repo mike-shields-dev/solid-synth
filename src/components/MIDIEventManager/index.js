2433237cfe0dc92091463f7f309aba31d10e0c6b
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-12-20 11:22:05 +0000

    feat: MidiEventManager triggers Attack and Release events on notes

--- src/components/MIDIEventManager/index.js
+++ src/components/MIDIEventManager/index.js
@@ -1,17 +1,62 @@
-import { onMount, onCleanup } from "solid-js";
+import { onMount, onCleanup, createSignal } from "solid-js";
 import { WebMidi } from "webmidi";
+import synth from "../../Synth";
 
-function MIDIEventManager(props) {
-  onMount(() => {
-    WebMidi.enable()
-      .then(() => {
-        console.log(WebMidi.inputs)
-        props.setMidiInputs(WebMidi.inputs);
-      })
-      .catch((e) => console.log(e));
+function MIDIEventManager() {
+  const [midiInputs, setMidiInputs] = createSignal([]);
+  const [midiOutputs, setMidiOutputs] = createSignal([]);
+
+  const onNoteEvent = (e) => {
+    const [statusByte, noteNumber, noteVelocity] = e.data;
+    console.table({ statusByte, noteNumber, noteVelocity });
+
+    let isActive;
+    if (e.type === "noteon") isActive = true;
+    if (e.type === "noteoff") isActive = false;
+
+    synth.updateNotes({ noteNumber, isActive });
+  };
+
+  const addListener = (midiInput, eventType) =>
+    midiInput.addListener(eventType, onNoteEvent);
+
+  const removeListener = (midiInput, eventType) =>
+    midiInput.removeListener(eventType, onNoteEvent);
+
+  const addListeners = (midiInputs) =>
+    midiInputs.forEach((midiInput) => {
+      addListener(midiInput, "noteon");
+      addListener(midiInput, "noteoff");
+    });
+
+  const removeListeners = (midiInputs) =>
+    midiInputs.forEach((midiInput) => {
+      removeListener(midiInput, "noteon");
+      removeListener(midiInput, "noteoff");
+    });
+
+  const updateMidiInsAndOuts = () => {
+    setMidiInputs((prevMidiInputs) => {
+      removeListeners(prevMidiInputs);
+      addListeners(WebMidi.inputs);
+      return WebMidi.inputs;
+    });
+    setMidiOutputs(WebMidi.outputs);
+  };
+
+  onMount(async () => {
+    try {
+      await WebMidi.enable({ sysex: true });
+      updateMidiInsAndOuts();
+      WebMidi.addListener("connected", updateMidiInsAndOuts);
+      WebMidi.addListener("disconnected", updateMidiInsAndOuts);
+    } catch (err) {
+      console.log(err);
+    }
   });
 
   onCleanup(() => WebMidi.disable());
+
   return null;
 }
 
